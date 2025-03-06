cb4806534a704f76e14fba4290f52b06e15d1588
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-03-07 00:30:36 +0530

    implement CRUD operations for User model in REST API

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const express = require('express');
 const app = express();
+const {User} = require('./models');
+
+app.use(express.json());
 
 app.listen(3001,(req,res)=>{
     console.log("Server is running on port http://localhost:3001");
 });
 
-app.get('/',(req,res)=>{
-    res.send("get request");
+app.get('/', async (req,res)=>{
+
+    const users = await User.findAll();
+    res.json(users);
+
+    // res.send("get request");
 });
-app.post('/',(req,res)=>{
-    res.send("post request");
+
+app.post('/', async (req,res)=>{
+
+    const newUser = await User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+    });
+    res.json(newUser);
+
+    // res.send("post request");
+});
+
+app.delete('/:id', async (req,res)=>{
+
+    const id = req.params.id;
+    const isDeleted = await User.destroy({
+        where:{
+            id:id
+        }
+    });
+    res.json(isDeleted);
+});
+
+app.put('/:id', async (req,res)=>{
+
+    const id = req.params.id;
+    const affectedRows = await User.update({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+    },{
+        where:{
+            id:id
+        }
+    });
+    res.json(affectedRows);
 });
