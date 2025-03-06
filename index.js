bb0eab3192746c22e12b10e9ddd4cfaee17eda18
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-03-07 00:13:09 +0530

    implement REST API with user model and database configuration

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+
+app.listen(3001,(req,res)=>{
+    console.log("Server is running on port http://localhost:3001");
+});
+
+app.get('/',(req,res)=>{
+    res.send("get request");
+});
+app.post('/',(req,res)=>{
+    res.send("post request");
+});
